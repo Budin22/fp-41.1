b7e68ee4805443f0d1e851ad71f4d91fd9fdcbc7
Budin <apserbud22@gmail.com>
2022-07-23 15:13:03 +0300

    done class

--- js/Class/Controller.js
+++ js/Class/Controller.js
@@ -1,15 +1,31 @@
-export const Controller = (model, view) => {
+export class Controller {
+    constructor(model, view) {
+        this.view = view
+        this.model = model
+        this.submitHandler = this.submitHandler.bind(this)
+        this.changeSubmitHandler = this.changeSubmitHandler.bind(this)
+        this.contactsHandler = this.contactsHandler.bind(this);
+        this.searchHandler = this.searchHandler.bind(this);
+        this.loadedHandler = this.loadedHandler.bind(this);
+        this.backBtnHandler = this.backBtnHandler.bind(this);
+    }
 
-    view.renderForm(model.containerId);
-    view.renderSearchInput(model.containerId);
-    view.renderNoContacts(model.containerId);
-    view.renderAddContactContainer(model.containerId);
+    start() {
+        this.view.renderForm(this.model.containerId);
+        this.view.renderSearchInput(this.model.containerId);
+        this.view.renderNoContacts(this.model.containerId);
+        this.view.renderAddContactContainer(this.model.containerId);
+        this.form = document.getElementById(this.model.formId)
+        this.search = document.getElementById(this.model.searchContainerId)
+        this.contactContainer = document.getElementById(this.model.contactsContainerId)
+        this.search.addEventListener('click', this.searchHandler);
+        this.search.addEventListener('input', this.searchHandler);
+        this.form.addEventListener('submit', this.submitHandler);
+        this.contactContainer.addEventListener('click', this.contactsHandler);
+        window.addEventListener('DOMContentLoaded', this.loadedHandler);
+    }
 
-    const form = document.getElementById(model.formId);
-    const search = document.getElementById(model.searchContainerId);
-    const contactContainer = document.getElementById(model.contactsContainerId);
-
-    const getDataForm = inputsCollection => {
+    getDataForm (inputsCollection) {
         if(inputsCollection instanceof NodeList) inputsCollection = Array.from(inputsCollection);
         return inputsCollection.reduce((acc, item) => {
                 try {
@@ -23,87 +39,85 @@ export const Controller = (model, view) => {
         );
     }
 
-    const submitHandler = event => {
+    submitHandler(event) {
         event.preventDefault();
         event.stopPropagation();
 
-        const inputs = form.querySelectorAll('input, textarea');
-        const data = getDataForm(inputs);
+        const inputs = this.form.querySelectorAll('input, textarea');
+        const data = this.getDataForm(inputs);
 
         if(!data) return alert("Нужно заполнить поля!");
 
-        model.setData = data;
-        view.clearContent(document.getElementById(model.contactsContainerId));
+        this.model.setData = data;
+        this.view.clearContent(document.getElementById(this.model.contactsContainerId));
 
-        const savedData = model.getData;
-        savedData.forEach(item => view.addContact(model.contactsContainerId, item));
+        const savedData = this.model.getData;
+        savedData.forEach(item => this.view.addContact(this.model.contactsContainerId, item));
 
-        form.reset();
-        view.remove(model.noContactsContainerId);
+        this.form.reset();
+        this.view.remove(this.model.noContactsContainerId);
 
-        if(model.getData.length === 1){
+        if(this.model.getData.length === 1){
 
-            const clearBtn = document.getElementById(model.searchBtnClearId);
-            const searchInput = document.getElementById(model.searchInputId);
+            const clearBtn = document.getElementById(this.model.searchBtnClearId);
+            const searchInput = document.getElementById(this.model.searchInputId);
 
             clearBtn.removeAttribute('disabled');
             searchInput.removeAttribute('disabled');
         }
     }
 
-    const changeSubmitHandler = event => {
+    changeSubmitHandler(event) {
         event.stopPropagation();
 
-        const inputs = document.getElementById(model.changeFormId).querySelectorAll('input, textarea');
-        const data = getDataForm(inputs);
+        const inputs = document.getElementById(this.model.changeFormId).querySelectorAll('input, textarea');
+        const data = this.getDataForm(inputs);
 
         if(!data) return alert("Нужно заполнить поля!");
-        model.setChangeData = data;
+        this.model.setChangeData = data;
     }
 
-    const backBtnHandler = (e) => {
+    backBtnHandler(e) {
         if(e.target.getAttribute('data-back')) window.location.reload();
     }
 
-    const contactsHandler = event => {
+    contactsHandler(event) {
         event.stopPropagation();
 
         const attribute = event.target.getAttribute('data-action');
-        const container = document.getElementById(model.containerId);
+        const container = document.getElementById(this.model.containerId);
         const targetId = +event.target.getAttribute('data-id');
-        const data = model.getData;
+        const data = this.model.getData;
 
         if(attribute === 'info') {
             const dataShow = data.filter(item => item.id === targetId);
 
-            view.clearContent(container);
-            view.renderInfoContact(model.containerId, dataShow[0]);
-            document.getElementById(model.containerId).addEventListener('click', backBtnHandler)
+            this.view.clearContent(container);
+            this.view.renderInfoContact(this.model.containerId, dataShow[0]);
+            document.getElementById(this.model.containerId).addEventListener('click', this.backBtnHandler)
 
         }
 
         if(attribute === 'change') {
             const dataShow = data.filter(item => item.id === targetId);
 
-            view.clearContent(container);
-            view.renderChangeForm(model.containerId, dataShow[0]);
+            this.view.clearContent(container);
+            this.view.renderChangeForm(this.model.containerId, dataShow[0]);
+            this.model.changeId = targetId;
 
-            model.changeId = targetId;
-            document.getElementById(model.changeFormId).addEventListener('submit', changeSubmitHandler)
-            document.getElementById(model.changeFormId).addEventListener('click', backBtnHandler)
-
+            document.getElementById(this.model.changeFormId).addEventListener('submit', this.changeSubmitHandler)
+            document.getElementById(this.model.changeFormId).addEventListener('click', this.backBtnHandler)
         }
 
         if(attribute === 'remove') {
             const oldData = data.filter(item => item.id !== targetId);
 
-
             event.target.closest('.cards-container').remove();
-            localStorage.setItem(model.formId, model.toJson(oldData));
+            localStorage.setItem(this.model.formId, this.model.toJson(oldData));
 
             if(data.length === 1) {
-                model.clearStorage();
-                view.renderNoContacts(model.containerId);
+                this.model.clearStorage();
+                this.view.renderNoContacts(this.model.containerId);
 
                 const clearBtn = document.getElementById('searchInputClear');
                 const searchInput = document.getElementById('searchInput');
@@ -113,28 +127,29 @@ export const Controller = (model, view) => {
         }
     }
 
-    const loadedHandler = () => {
-        const data = model.getData;
+    loadedHandler() {
+        const data = this.model.getData;
 
         if(data && data.length) {
-            view.remove(model.noContactsContainerId);
-            data.forEach(data => view.addContact(model.contactsContainerId, data));
+            this.view.remove(this.model.noContactsContainerId);
+            data.forEach(data => this.view.addContact(this.model.contactsContainerId, data));
 
-            const clearBtn = document.getElementById(model.searchBtnClearId);
-            const searchInput = document.getElementById(model.searchInputId);
+            const clearBtn = document.getElementById(this.model.searchBtnClearId);
+            const searchInput = document.getElementById(this.model.searchInputId);
             clearBtn.removeAttribute('disabled');
             searchInput.removeAttribute('disabled');
         }
     }
 
-    const searchHandler = event => {
+    searchHandler(event) {
         event.preventDefault();
         event.stopPropagation();
+
         const valueNumber = event.target.value.match(/\d/g)?.join('');
         const valueString = event.target.value.match(/\D/gi)?.join('');
         const regexpNum = new RegExp(`${valueNumber}`, 'g');
 
-        localStorage.setItem('search', JSON.stringify(model.getData))
+        localStorage.setItem('search', JSON.stringify(this.model.getData))
 
         if(event.target instanceof HTMLInputElement) {
 
@@ -154,22 +169,16 @@ export const Controller = (model, view) => {
                 if(data.length > newData.length) localStorage.setItem('search', JSON.stringify(newData));
             }
 
-            view.remove(model.contactsContainerId);
-            view.renderAddContactContainer(model.containerId);
+            this.view.remove(this.model.contactsContainerId);
+            this.view.renderAddContactContainer(this.model.containerId);
 
-            const contactContainer = document.getElementById(model.contactsContainerId);
-            contactContainer.addEventListener('click', contactsHandler);
+            const contactContainer = document.getElementById(this.model.contactsContainerId);
+            contactContainer.addEventListener('click', this.contactsHandler);
 
             const savedData = JSON.parse(localStorage.getItem('search'));
-            if(savedData.length) savedData.forEach(item => view.addContact(model.contactsContainerId, item));
+            if(savedData.length) savedData.forEach(item => this.view.addContact(this.model.contactsContainerId, item));
         }
 
         if(event.target instanceof HTMLButtonElement) document.getElementById('searchInput').value = '';
     }
-
-    search.addEventListener('click', searchHandler);
-    search.addEventListener('input', searchHandler);
-    form.addEventListener('submit', submitHandler);
-    contactContainer.addEventListener('click', contactsHandler);
-    window.addEventListener('DOMContentLoaded', loadedHandler);
 }
--- js/index.js
+++ js/index.js
@@ -3,4 +3,5 @@ import { Controller } from "./Class/Controller.js";
 import { Model } from "./Class/Model.js";
 import { View } from "./Class/View.js";
 
-Controller(new Model(), new View());
+const app = new Controller(new Model(), new View());
+app.start()
